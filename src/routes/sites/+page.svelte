d13a1b419c192e85ba60edaf640fe6b925b9084b
Blake Prejean <xerosgame@hotmail.com>
2024-04-10 08:42:43 -0700

    fixed sites page not having table headers

--- src/routes/sites/+page.svelte
+++ src/routes/sites/+page.svelte
@@ -18,7 +18,11 @@
     <h3 class="flex text-2xl font-bold p-1 mb-3">
       All Sites <p class="px-2 ml-3 bg-cscol-000 rounded-md">{data.sites.length}</p>
     </h3>
-    <FilteredTable columns={["ID", "Name", "Company"]}>
+    <FilteredTable columns={[
+      { label: "ID", filter: "Text" },
+      { label: "Name", filter: "Text" },
+      { label: "Company", filter: "Text" },
+    ]}>
       {#each data.sites as site, index}
         <FilteredRow
           index={index}
