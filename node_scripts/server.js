4a18b6ec0aef691d63d68dedcb34219e19beaf17
Mythidas <xerosgame@hotmail.com>
2024-06-09 16:42:21 -0700

    Added scheduled node instance

---
+++ node_scripts/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import schedule from "node-schedule";
+
+const app = express();
+const port = 2304;
+
+const rule = new schedule.RecurrenceRule();
+rule.minute = 59;
+rule.hour = 23;
+
+// Sync devices at midnight everyday
+const job = schedule.scheduleJob(rule, () => {
+  fetch("https://localhost:2301/api/v2/devices/sync", {
+    method: "POST"
+  });
+})
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
