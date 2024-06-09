7bc632346bab32eaa9419855c9e7e00cacec224d
Mythidas <xerosgame@hotmail.com>
2024-06-09 16:20:02 -0700

    Fixed sign in routing for auth

---
+++ src/routes/(auth)/+layout.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ locals, url }) {
+  const session = await locals.auth();
+  if (session) {
+    return redirect(302, "/sites");
+  }
+
+  return session;
+}
