99fd207239cee688f5b3a97a12f97434d3d65eed
Blake Prejean <xerosgame@hotmail.com>
2024-06-19 15:59:24 -0700

    Fixed table height on device purge

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -11,12 +11,12 @@
 {#if open}
 <div class="modal z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center p-8 lg:p-0">
   <div class="modal-overlay fixed w-full h-full bg-base-100 bg-opacity-75"></div>
-  <div class="flex flex-col z-50 w-1/2 h-2/3 p-2 mx-auto justify-between space-y-2 bg-base-200 shadow-xl overflow-y-auto">
+  <div class="flex flex-col z-50 w-1/2 h-2/3 p-2 mx-auto justify-between space-y-2 bg-base-200 shadow-xl">
     <div class="head bg-gray-100 p-2 space-y-2 text-2xl font-bold">
       <p>{title}</p>
       <hr />
     </div>
-    <div class="content h-full space-y-2 p-2">
+    <div class="content h-full space-y-2 p-2 overflow-hidden">
       <slot />
       <hr />
     </div>
