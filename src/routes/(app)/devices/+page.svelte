bd4b566812d81e4eedf412733991550a634bdf84
Blake Prejean <xerosgame@hotmail.com>
2024-07-01 16:12:56 -0700

    Added device table prefab

--- src/routes/(app)/devices/+page.svelte
+++ src/routes/(app)/devices/+page.svelte
@@ -1,54 +1,7 @@
 <script lang="ts">
-  import Table from "$lib/components/table.svelte";
-
-  let total_items = 1;
-  let filtered_data: any[];
-  let selected_row: number;
-
-  $: if (filtered_data) {
-    for (let i = 0; i < filtered_data.length; i++) {
-      filtered_data[i].error = [];
-      filtered_data[i].warn = [];
-
-      if (!filtered_data[i]["heartbeat_av"]) {
-        filtered_data[i].error.push("heartbeat_av");
-
-      } else if (!filtered_data[i]["heartbeat_rmm"]) {
-        filtered_data[i].error.push("heartbeat_rmm")
-
-      } else if (new Date(filtered_data[i]["heartbeat_av"]).getTime() <= new Date(Date.now()).getTime() - (1000 * 3600 * 24 * 30)) {
-        filtered_data[i].warn.push("heartbeat_av")
-
-      } else if (new Date(filtered_data[i]["heartbeat_rmm"]).getTime() <= new Date(Date.now()).getTime() - (1000 * 3600 * 24 * 30)) {
-        filtered_data[i].warn.push("heartbeat_rmm");
-      }
-    }
-  }
+  import DeviceTable from "$lib/prefabs/device_table.svelte";
 </script>
 
 <div class="flex w-full h-full p-2 bg-base-200">
-  <Table
-    columns={[
-      { key: "hostname", name: "Name", group: "Device", default: "-", type: "Text" },
-      { key: "title", name: "Site", group: "Site", default: "-", type: "Text" },
-      { key: "company_title", name: "Company", group: "Company", default: "-", type: "Text" },
-      { key: "os", name: "OS", group: "Device", default: "-", type: "Text" },
-      { key: "heartbeat_rmm", name: "VSA Heartbeat", group: "DeviceRMM", default: "-", type: "Date" },
-      { key: "heartbeat_av", name: "Sophos Heartbeat", group: "DeviceAV", default: "-", type: "Date" },
-      { key: "ipv4", name: "LAN", group: "Device", default: "-", type: "Text" },
-      { key: "wan", name: "WAN", group: "Device", default: "-", type: "Text" },
-      { key: "tamper", name: "Tamper Protection", group: "DeviceAV", default: "-", type: "Bool" },
-      { key: "health", name: "Sophos Health", group: "DeviceAV", default: "-", type: "Text" },
-      { key: "memory", name: "Memory Total", group: "DeviceRMM", default: "-", type: "Number" },
-      { key: "uac", name: "UAC Enabled", group: "DeviceRMM", default: "-", type: "Bool" },
-      { key: "firewall", name: "Windows Firewall", group: "DeviceRMM", default: "-", type: "Bool" },
-    ]}
-    data={`/api/v2/devices`}
-    bind:total_items
-    page={1}
-    count={25}
-    bind:filtered_data
-    bind:selected_row
-    sticky_first
-  />
+  <DeviceTable />
 </div>
--- src/routes/(app)/sites/[slug]/+page.svelte
+++ src/routes/(app)/sites/[slug]/+page.svelte
@@ -1,38 +1,16 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import Table from "$lib/components/table.svelte";
   import type { Site } from "$lib/interfaces/i_db";
+  import DeviceTable from "$lib/prefabs/device_table.svelte";
 
   export let data: { site: Site };
 
-  let total_items = 1;
-  let page = 1;
-  let count = 25;
   let filtered_data: any[];
   let selected_row: number;
 
   $: site_id = data.site.site_id;
   $: if (selected_row > -1) {
     goto(`/sites/${site_id}/${filtered_data[selected_row].device_id}`);
-  }
-  $: if (filtered_data) {
-    for (let i = 0; i < filtered_data.length; i++) {
-      filtered_data[i].error = [];
-      filtered_data[i].warn = [];
-
-      if (!filtered_data[i]["heartbeat_av"]) {
-        filtered_data[i].error.push("heartbeat_av");
-
-      } else if (!filtered_data[i]["heartbeat_rmm"]) {
-        filtered_data[i].error.push("heartbeat_rmm")
-
-      } else if (new Date(filtered_data[i]["heartbeat_av"]).getTime() <= new Date(Date.now()).getTime() - (1000 * 3600 * 24 * 30)) {
-        filtered_data[i].warn.push("heartbeat_av")
-
-      } else if (new Date(filtered_data[i]["heartbeat_rmm"]).getTime() <= new Date(Date.now()).getTime() - (1000 * 3600 * 24 * 30)) {
-        filtered_data[i].warn.push("heartbeat_rmm");
-      }
-    }
   }
 </script>
 
@@ -42,25 +20,5 @@
   <p>{data.site.title}</p>
 </h3>
 <div class="flex flex-col w-full h-full p-2 bg-base-200 overflow-hidden">
-  <Table
-    columns={[
-      { key: "hostname", name: "Name", group: "Device", default: "-", type: "Text" },
-      { key: "os", name: "OS", group: "Device", default: "-", type: "Text" },
-      { key: "heartbeat_rmm", name: "VSA Heartbeat", group: "DeviceRMM", default: "-", type: "Date" },
-      { key: "heartbeat_av", name: "Sophos Heartbeat", group: "DeviceAV", default: "-", type: "Date" },
-      { key: "ipv4", name: "LAN", group: "Device", default: "-", type: "Text" },
-      { key: "wan", name: "WAN", group: "Device", default: "-", type: "Text" },
-      { key: "firewall", name: "Windows Firewall", group: "DeviceRMM", default: "-", type: "Bool" },
-      { key: "uac", name: "UAC Enabled", group: "DeviceRMM", default: "-", type: "Bool" },
-      { key: "memory", name: "Memory Total", group: "DeviceRMM", default: "-", type: "Text" },
-      { key: "tamper", name: "Tamper Protection", group: "DeviceAV", default: "-", type: "Bool" },
-      { key: "health", name: "Sophos Health", group: "DeviceAV", default: "-", type: "Text" },
-    ]}
-    data={`/api/v2/devices/${site_id}`}
-    bind:total_items
-    bind:page
-    bind:count
-    bind:filtered_data
-    bind:selected_row
-  />
+  <DeviceTable bind:selected_row site_id={site_id}/>
 </div>
