6cc0453589be53af2263e2abb253c8bffa28ee79
Blake Prejean <xerosgame@hotmail.com>
2024-08-14 15:04:38 -0700

    Modified sophos page

--- src/routes/(app)/sites/[slug]/av/[id]/+page.svelte
+++ src/routes/(app)/sites/[slug]/av/[id]/+page.svelte
@@ -15,11 +15,11 @@
     <p>{data.av_device.hostname}</p>
   </div>
 </div>
-<div class="flex flex-col w-full h-full p-3 bg-theme-dark-200/75 rounded-md shadow-md">
+<div class="flex flex-col w-fit h-fit p-3 bg-theme-dark-200/75 rounded-md shadow-md">
   <h3 class="text-3xl pb-5">{data.av_device.hostname}</h3>
   <div class="flex space-x-0.5 text-lg">
     <div class="space-y-0.5">
-      <p class="bg-theme-dark-300 px-2 py-1">OS</p>
+      <p class="bg-theme-dark-300 px-2 py-1 rounded-tl">OS</p>
       <p class="bg-theme-dark-300 px-2 py-1">Last Online</p>
       <p class="bg-theme-dark-300 px-2 py-1">Tamper Protection</p>
       <p class="bg-theme-dark-300 px-2 py-1">Tamper Password</p>
@@ -27,10 +27,10 @@
       <p class="bg-theme-dark-300 px-2 py-1">Last Known IP</p>
       <p class="bg-theme-dark-300 px-2 py-1">MAC Address</p>
       <p class="bg-theme-dark-300 px-2 py-1">Health</p>
-      <p class="bg-theme-dark-300 px-2 py-1">Isolation</p>
+      <p class="bg-theme-dark-300 px-2 py-1 rounded-bl">Isolation</p>
     </div>
     <div class="space-y-0.5">
-      <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.os.name}</p>
+      <p class="bg-theme-dark-300 px-2 py-1 rounded-tr">{data.av_device.os.name}</p>
       <p class="bg-theme-dark-300 px-2 py-1">{new Time(data.av_device.lastSeenAt).get_time_since()}</p>
       <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.tamperProtectionEnabled ? "Enabled" : "Disabled"}</p>
       <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.tamper_info?.password || "Unknown"}</p>
@@ -38,7 +38,7 @@
       <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.ipv4Addresses[0] || ""}</p>
       <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.macAddresses[0] || ""}</p>
       <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.health.overall.toUpperCase()}</p>
-      <p class="bg-theme-dark-300 px-2 py-1">{data.av_device.isolation.adminIsolated || data.av_device.isolation.selfIsolated ? "Enabled" : "Disabled"}</p>
+      <p class="bg-theme-dark-300 px-2 py-1 rounded-br">{data.av_device.isolation.adminIsolated || data.av_device.isolation.selfIsolated ? "Enabled" : "Disabled"}</p>
     </div>
   </div>
 </div>
